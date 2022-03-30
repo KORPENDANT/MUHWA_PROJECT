9948fd0ba4a6238db0c8b8c0c7604d43200710f6
unknown <ijeonghu60@gmail.com>
2022-03-30 18:47:00 +0900

    버전 0.2

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from unicodedata import name
 from xml.etree.ElementInclude import include
 from django.urls import path,include
-from .views import IndexView
+from .views import IndexView, StoryView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -9,6 +9,7 @@ app_name = 'home'
 
 urlpatterns = [
     path('', IndexView.as_view(),name ='index'),
+    path('story/',StoryView.as_view(),name="story")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
